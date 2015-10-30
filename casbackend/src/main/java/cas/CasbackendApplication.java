334c3e17cf4e3d3018cdfe3d1704795c2905bf23
Gabriel Humphrey <gabe2390@gmail.com>
2015-10-30 19:33:28 -0400

    Added Spring Boot project and Ionic Project

---
+++ casbackend/src/main/java/cas/CasbackendApplication.java
@@ -0,0 +1,12 @@
+package cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasbackendApplication.class, args);
+    }
+}
